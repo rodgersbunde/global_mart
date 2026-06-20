1758a9fe7d1caeac1e916b46815b752b28a950b4
Rodgers <roggybunde@gmail.com>
2026-06-20 13:37:46 +0300

    initial commit

--- target/run/globalmart/models/fact/fact_orders.sql
+++ target/run/globalmart/models/fact/fact_orders.sql
@@ -5,7 +5,7 @@
         delete from "postgres"."postgres"."fact_orders" as DBT_INTERNAL_DEST
         where (order_id) in (
             select distinct order_id
-            from "fact_orders__dbt_tmp125527256356" as DBT_INTERNAL_SOURCE
+            from "fact_orders__dbt_tmp133726805582" as DBT_INTERNAL_SOURCE
         );
 
     
@@ -13,6 +13,6 @@
     insert into "postgres"."postgres"."fact_orders" ("order_id", "product_id", "region_id", "state_id", "category_id", "subcategory_id", "order_date", "ship_date", "sales", "quantity", "discount", "profit", "profit_margin")
     (
         select "order_id", "product_id", "region_id", "state_id", "category_id", "subcategory_id", "order_date", "ship_date", "sales", "quantity", "discount", "profit", "profit_margin"
-        from "fact_orders__dbt_tmp125527256356"
+        from "fact_orders__dbt_tmp133726805582"
     )
   
